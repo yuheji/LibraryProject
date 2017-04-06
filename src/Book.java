1ef20dcd8fff7a252a105597b8a3586f223fcd8e
Joshua Ji <josh@enthrallsports.com>
2017-04-05 17:11:19 -0700

    Initial commit

---
+++ src/Book.java
@@ -0,0 +1,9 @@
+/**
+ * Model for Book object to be saved, edited, and retrieved from database
+ */
+public class Book {
+    int id;
+    String title;
+    String author;
+    String description;
+}
